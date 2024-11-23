4d9433117ccde4c55672f644fe0e68922f9be4cd
LudoC <ludo.agtm@outlook.fr>
2024-11-23 16:55:16 +0100

    Regroup CSS in  file

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,7 @@
-import './Home.css';
-
 function Home() {
   return (
-    <div className="Home">
-      <header className="App-header">
-        <p>
-          Site internet de John Doe
-        </p>
-      </header>
+    <div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './component/style.css';
 import Home from './Home';
 import Header from './component/header';
 import Footer from './component/footer';
