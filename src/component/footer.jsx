1f161f0826c92aa048c177c32416b2104bb82252
LudoC <ludo.agtm@outlook.fr>
2024-11-24 19:04:21 +0100

    Add nofollow and noreferrer for externals links

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -10,16 +10,16 @@ export default function Footer() {
                             <p className="m-auto"><strong className="fs-4">John Doe</strong></p>
                             <adress className="m-auto">
                                 <a href="https://www.google.com/maps/place/40+Rue+Laure+Diebold,+69009+Lyon/@45.7786656,4.794108,16z/data=!3m1!4b1!4m6!3m5!1s0x47f4eb65edac5b3f:0xe01c47049cb2e2b9!8m2!3d45.778662!4d4.7989789!16s%2Fg%2F11c24790th?entry=ttu&g_ep=EgoyMDI0MTExOS4yIKXMDSoJLDEwMjExMjM0SAFQAw%3D%3D"
-                                rel="nofollow" target="_blank" className="text-light link-underline link-underline-opacity-0">
+                                 rel="noreferrer nofollow" target="_blank" className="text-light link-underline link-underline-opacity-0">
                                 40 Rue Laure Diebold<br/>
                                 69009 Lyon, France</a><br/>
                                 <a href="tel:0606060606" className="text-light link-underline link-underline-opacity-0">06 06 06 06 06</a><br/>
                                 <a href="mailto:john.doe@gmail.com" className="text-light link-underline link-underline-opacity-0">john.doe@gmail.com</a>
                             </adress>
                             <div className="my-2">
-                                <a href="https://github.com/luddu73" rel="nofollow" target="_blank" className="text-light"><i className="bi bi-github socialnetwork"></i></a> 
-                                <a href="https://x.com/LudovicCarril" rel="nofollow" target="_blank" className="text-light"><i className="bi bi-twitter socialnetwork"></i></a>
-                                <a href="https://fr.linkedin.com/in/ludovic-carril-377b34116" rel="nofollow" target="_blank" className="text-light"><i className="bi bi-linkedin socialnetwork"></i></a>
+                                <a href="https://github.com/luddu73"  rel="noreferrer nofollow" target="_blank" className="text-light"><i className="bi bi-github socialnetwork"></i></a> 
+                                <a href="https://x.com/LudovicCarril"  rel="noreferrer nofollow" target="_blank" className="text-light"><i className="bi bi-twitter socialnetwork"></i></a>
+                                <a href="https://fr.linkedin.com/in/ludovic-carril-377b34116"  rel="noreferrer nofollow" target="_blank" className="text-light"><i className="bi bi-linkedin socialnetwork"></i></a>
                             </div>
                         </div>
                         <div className="col-md-3 p-1">
